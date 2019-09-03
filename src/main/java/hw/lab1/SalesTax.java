9557bc1e42788fe6c6bae4a2fa569c7b8970aa3f
0800490 <0800490@KELCE-LABS18-P.pittstate.edu>
2019-09-03 12:12:55 -0500

    initial commit with code

---
+++ src/main/java/hw/lab1/SalesTax.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw.lab1;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 0800490
+ */
+public class SalesTax {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        JOptionPane.showMessageDialog(null, "Please enter purchase amount");
+        String purchasePriceAsString = JOptionPane.showInputDialog("Purchase amount");
+        float purchasePriceAsFloat = Float.parseFloat(purchasePriceAsString);
+        
+        
+        float countryTaxRate = .02F;
+        float stateTaxRate = .04F;
+        
+        float countrySalesTax = purchasePriceAsFloat * countryTaxRate;
+        float stateSalesTax = purchasePriceAsFloat * stateTaxRate;
+        
+        float totalSalesTax = countrySalesTax + stateSalesTax;
+        float totalOfSale = purchasePriceAsFloat + totalSalesTax;
+        
+        System.out.println("The amount before tax is " + purchasePriceAsFloat + " the state tax is " + 
+               stateSalesTax + " the country sales tax is " + countrySalesTax + " the total sales tax is " 
+        + totalSalesTax + " the total price of sale is " + totalOfSale);
+        
+    }
+}
