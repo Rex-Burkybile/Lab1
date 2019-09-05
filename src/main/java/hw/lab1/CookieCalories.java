6f8cc462fa1a087044bdb1a264ebdea7c3d68fac
0800490 <0800490@KELCE-LABS18-P.pittstate.edu>
2019-09-05 11:21:48 -0500

    finished assignment

--- src/main/java/hw/lab1/CookieCalories.java
+++ src/main/java/hw/lab1/CookieCalories.java
@@ -5,10 +5,24 @@
  */
 package hw.lab1;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 0800490
  */
 public class CookieCalories {
-    
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        JOptionPane.showMessageDialog(null, "How many cookies did you eat");
+        String CookiesAteAsString = JOptionPane.showInputDialog("Number of cookies");
+        float CookiesAteAsFloat = Float.parseFloat(CookiesAteAsString);
+        
+        float servings = CookiesAteAsFloat/4 ;
+        float calories = servings * 300;
+        
+        System.out.println("You consumed " + calories + " calories");  
+    }
 }
