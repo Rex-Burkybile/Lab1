4bd610dad2713232d0bcfd5eace154841ff06043
0800490 <0800490@KELCE-LABS18-P.pittstate.edu>
2019-09-05 11:33:29 -0500

    Formatted nicer

--- src/main/java/hw/lab1/SalesTax.java
+++ src/main/java/hw/lab1/SalesTax.java
@@ -14,28 +14,27 @@ import javax.swing.JOptionPane;
  * @author 0800490
  */
 public class SalesTax {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        
+
         JOptionPane.showMessageDialog(null, "Please enter purchase amount");
         String purchasePriceAsString = JOptionPane.showInputDialog("Purchase amount");
         float purchasePriceAsFloat = Float.parseFloat(purchasePriceAsString);
-        
-        
+
         float countryTaxRate = .02F;
         float stateTaxRate = .04F;
-        
+
         float countrySalesTax = purchasePriceAsFloat * countryTaxRate;
         float stateSalesTax = purchasePriceAsFloat * stateTaxRate;
-        
+
         float totalSalesTax = countrySalesTax + stateSalesTax;
         float totalOfSale = purchasePriceAsFloat + totalSalesTax;
-        
+
         NumberFormat money = NumberFormat.getCurrencyInstance();
-        System.out.println("The amount before tax is " + money.format (purchasePriceAsFloat) + " the state tax is " + 
-               money.format (stateSalesTax) + " the country sales tax is " + money.format (countrySalesTax) + " the total sales tax is " 
-        + money.format (totalSalesTax) + " the total price of sale is " + money.format (totalOfSale));
-        
+        System.out.println("The amount before tax is " + money.format(purchasePriceAsFloat) + " the state tax is "
+                + money.format(stateSalesTax) + " the country sales tax is " + money.format(countrySalesTax) + " the total sales tax is "
+                + money.format(totalSalesTax) + " the total price of sale is " + money.format(totalOfSale));
+
     }
 }
